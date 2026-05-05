722ef56fba966daf6490c6d137847d23b8ac675f
Shubhanshu Mishra <shivamamethiup@gmail.com>
2026-05-06 01:38:26 +0530

    Refactor buy function and remove cart logic
    Removed cart functionality and updated buy function to alert on purchase.

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-let cart = [];
-
-function buy(product){
-  cart.push(product);
-
-  document.getElementById("cart-count").innerText = cart.length;
-
-  let list = document.getElementById("cart-items");
-  let item = document.createElement("li");
-  item.innerText = product;
-  list.appendChild(item);
-
-  alert(product + " added to cart 🚀");
-}
-
-function showCart(){
-  document.getElementById("cart-box").style.display = "block";
+function buy(plan){
+  alert(plan + " purchased successfully 🚀");
 }
